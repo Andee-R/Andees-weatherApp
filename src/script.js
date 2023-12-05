05912ef80e835a1437e8f0d9be6e193d2c185551
Andee-R <aqueen.reid@gmail.com>
2023-12-04 19:49:30 -0700

    added forecast to JS (not working)

--- src/script.js
+++ src/script.js
@@ -52,6 +52,34 @@ function searchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="row">
+    <div class="col-2">
+    <div class="weather-forecast-data">
+    <div class="weather-forecast-day">${day}</div>
+    <div class="weather-forecast-icon">⛅<div/>
+    <div class= "weather-forecast-temperatures"
+    <span class="weather-forecast-temperature-max">66°</span>
+    <span class="weather-forecast-temperature-min">46°</span>
+    </div>
+    </div>
+    </div>
+    </div>
+    `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHtml = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
 searchCity("Glendale");
+displayForecast();
