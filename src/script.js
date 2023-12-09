f99ef72e234dbee8811039ac7c529782fbf96283
Andee-R <aqueen.reid@gmail.com>
2023-12-08 22:21:09 -0700

    working on JS forecast api (appearing vertical)

--- src/script.js
+++ src/script.js
@@ -14,11 +14,13 @@ function refreshWeather(response) {
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img
   src="${response.data.condition.icon_url}"
   class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let hours = date.getHours();
@@ -51,28 +53,43 @@ function searchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+function getForecast(city) {
+  let apiKey = "c9b605ad7a94bt8873a8fdebe09251o1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-      <div class="row">
-    <div class="col-2">
-    <div class="weather-forecast">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">⛅<div/>
-    <div class= "weather-forecast-temperatures"
-    <span class="weather-forecast-temperature-max"><strong>66°</strong></span>
-    <span class="weather-forecast-temperature-min">46°</span>
-    </div>
-    </div>
-    </div>
-    </div>
-    `;
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="row">
+        <div class="col-2">
+        <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <img src="${day.condition.icon_url}"class="weather-forecast-icon";
+      <div class= "weather-forecast-temperatures"
+      <span class="weather-forecast-temperature-max"><strong>${Math.round(
+        day.temperature.maximum
+      )}°</strong></span>
+        <span class="weather-forecast-temperature-min">${Math.round(
+          day.temperature.minimum
+        )}°</span>
+        </div>
+        </div>
+        </div>
+        </div>
+          `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
@@ -82,4 +99,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
 
 searchCity("Glendale");
-displayForecast();
