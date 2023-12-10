a757bf8886bca5df120a261290a02e651692c2c7
Andee-R <aqueen.reid@gmail.com>
2023-12-10 16:53:09 -0700

    fixed forecast in js and css

--- src/script.js
+++ src/script.js
@@ -77,15 +77,13 @@ function displayForecast(response) {
         `
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
-        <img
+        <img class="weather-forecast-icon"
           src="${forecastDay.condition.icon_url}"
-          alt=""
-          width="52px"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
+          <span class="weather-forecast-temperature-max"> <strong> ${Math.round(
             forecastDay.temperature.maximum
-          )}°</span>
+          )}°</strong></span>
           <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temperature.minimum
           )}°</span>
